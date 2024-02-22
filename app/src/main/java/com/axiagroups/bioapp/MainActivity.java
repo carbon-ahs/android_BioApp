9da777aec0d9edaa25caac3352eedc87b3d94896
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-02-22 17:45:51 +0600

    data binding added

--- app/src/main/java/com/axiagroups/bioapp/MainActivity.java
+++ app/src/main/java/com/axiagroups/bioapp/MainActivity.java
@@ -1,14 +1,20 @@
 package com.axiagroups.bioapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.axiagroups.bioapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+//        binding.textView
     }
 }
